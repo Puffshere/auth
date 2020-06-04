8b3fe3bd6dd496ebc12a2888d0357df773daa76a
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-06-04 14:11:38 -0700

    this is where it breaks

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+
   return {
-    ...createStore(rootReducer)
+    ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
+    runSaga: sagaMiddleware.run(rootSaga)
   }
 };
 
