446fd53359c5e29ccd8eed43e50df77aa7f80d30
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-06-04 13:54:43 -0700

    adds register link

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from '../container/App';
+import App from '../containers/App';
 import RegisterPage from '../components/registerPage';
+import LoginPage from '../components/loginPage';
 
 export default (
   <Route path='/' component={App}>
-    <IndexRoute component={RegisterPage} />
+    <IndexRoute component={LoginPage} />
+    <Route path='login' component={LoginPage} />
     <Route path='register' component={RegisterPage} />
   </Route>
 );
