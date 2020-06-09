f04163b5833995ea38b44240eeb86dbadfa4306b
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-06-08 21:11:25 -0700

    adds date component

--- src/components/dashboardPage.js
+++ src/components/dashboardPage.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+const CurrentDate = (props) => {
+  return (
+    <div>
+      <p>The current date is: {props.date}</p>
+    </div>
+  );
+};
+
 class DashboardPage extends Component {
   render() {
     return (
       <div className='dashboard'>
         <h1 className='loginPageTitle'>BladeX</h1>
         <h3 className='dashboardTitle'>Dashboard</h3>
+        <CurrentDate date={Date()}/>
       </div>
     );
   }
