de8c484263cfe41264f53a23399dc9fcd1145c80
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-06-10 18:34:10 -0700

    adds update state button

--- src/components/fixedBladesList.js
+++ src/components/fixedBladesList.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 class FixedBladesList extends React.Component {
     constructor(props) {
       super(props);
-  
+      this.state = {
+        name: "Hello"
+      };
+      this.handleClick = this.handleClick.bind(this);
     }
+    handleClick() {
+    this.setState({
+      name: 'Goodbye'
+    });
+  }
     render() {
+    
       return (
           <div>
+            <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
               { /* change code below this line */ }
               <p>Your Fixed blades are: <strong>{this.props.fixedList}</strong></p>
               { /* change code above this line */ }
