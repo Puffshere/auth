68f266daa20f54cc093c8a34e11d63b9d9304f35
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-06-21 05:33:07 -0700

    adds table styling

--- src/components/GetLocalPosts.js
+++ src/components/GetLocalPosts.js
@@ -1,33 +1,52 @@
 import React, { Component } from 'react';
 
 import knives from '../components/mockData';
-// get posts from online api
-// it's return a json file
 class GetLocalPosts extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {            
-            knives :knives            
+        this.state = {
+            knives: knives
         };
     }
 
     render() {
-        const {knives} = this.state;
-        return(
+        const { knives } = this.state;
+        return (
             <div>
+                {/* <table>
+                    <thead>
+                        <tr>
+                            <th><h4>Knife</h4></th>
+                            <th><h4>Blade Shape</h4></th>
+                            <th><h4>Steel</h4></th>
+                            <th><h4>Price</h4></th>
+                        </tr>
+                    </thead>
+                </table> */}
                 <ol className="item">
-                {
-                    knives.map(knife => (
-                        <div key={knife.id} align="start">
-                                <tr className="body">{knife.id} {knife.brand} {knife.model}</tr>
-                                
-                        </div>
-                    ))
-                }
+                    {
+                        knives.map(knife => (
+                            <div key={knife.id} align="start">
+                                <table className='container showStopper'>
+                                    <thead>
+                                        <tr></tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td className='show shane'>{knife.brand} {knife.model}</td>
+                                            <td className='show shane'>{knife.bladeShape}</td>
+                                            <td className='show shane'>{knife.steel}</td>
+                                            <td className='show shane'>{knife.price}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        ))
+                    }
                 </ol>
             </div>
         );
     }
-  }
-  
-  export default GetLocalPosts;
+}
+
+export default GetLocalPosts;
--- src/components/mockData.js
+++ src/components/mockData.js
@@ -4,42 +4,60 @@ const knives = [
         "id": 1,
         "model": "Bugout",
         "forSale": true,
-        "brand": "Benchmade"
+        "brand": "Benchmade",
+        "steel": "D2",
+        "bladeShape": "Drop Point",
+        "price": "$109.99"
       },
       {
         "userId": 1,
         "id": 2,
         "model": "940",
         "forSale": false,
-        "brand": "Benchmade"
+        "brand": "Benchmade",
+        "steel": "AUS8",
+        "bladeShape": "Spear Point",
+        "price": "$179.99"
       },
       {
         "userId": 1,
         "id": 3,
         "model": "Atmos",
         "forSale": true,
-        "brand": "Benchmade"
+        "brand": "Kershaw",
+        "steel": "LMAX",
+        "bladeShape": "Clip Point",
+        "price": "$29.99"
       },
       {
         "userId": 1,
         "id": 4,
         "model": "Sebenza21",
         "forSale": false,
-        "brand": "Benchmade"
+        "brand": "Chris Reeves",
+        "steel": "CPM S35VN",
+        "bladeShape": "Tanto",
+        "price": "$499.99"
       },
       {
         "userId": 1,
         "id": 5,
         "model": "Griptilian",
         "forSale": true,
-        "brand": "Benchmade"
+        "brand": "Benchmade",
+        "steel": "M390",
+        "bladeShape": "Clip Point",
+        "price": "$89.99"
       },
       {
         "userId": 1,
         "id": 6,
         "model": "Leek",
         "forSale": true,
-        "brand": "Benchmade"
+        "brand": "Kershawn",
+        "steel": "H1",
+        "bladeShape": "Tanto",
+        "price": "$49.99"
       }
     ]
 
