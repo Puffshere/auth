c1a18303c1eabc23720e5a91fa7071ab6acb34ee
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-06-27 16:05:42 -0700

    s

--- src/components/dropDown.js
+++ src/components/dropDown.js
@@ -1,66 +1,66 @@
-// import React from 'react';
-// import './style.css';
-// import ReactDOM from 'react-dom';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import './style.css';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
-// class Dropdown extends React.Component {
-//   constructor() {
-//     super();
+class Dropdown extends React.Component {
+  constructor() {
+    super();
 
-//     this.state = {
-//       displayMenu: false,
-//     };
+    this.state = {
+      displayMenu: false,
+    };
 
-//     this.showDropdownMenu = this.showDropdownMenu.bind(this);
-//     this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
-//   };
+    this.showDropdownMenu = this.showDropdownMenu.bind(this);
+    this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+  };
 
-//   showDropdownMenu(event) {
-//     event.preventDefault();
-//     this.setState({ displayMenu: true }, () => {
-//       document.addEventListener('click', this.hideDropdownMenu);
-//     });
-//   }
+  showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
 
-//   hideDropdownMenu() {
-//     this.setState({ displayMenu: false }, () => {
-//       document.removeEventListener('click', this.hideDropdownMenu);
-//     });
-//   }
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+  }
 
-//   render() {
-//     return (
-//       <div className="dropdown">
-//         <div className='dropdownStyling' onClick={this.showDropdownMenu}>Your Knives</div>
+  render() {
+    return (
+      <div className="dropdown">
+        <div className='dropdownStyling' onClick={this.showDropdownMenu}>Your Knives</div>
 
-//         {this.state.displayMenu ? (
-//           <div className='help1'>
-//           <ul>
-//             <li className='help'><a href="dashboard">Dashboard</a></li>
-//             <li className='help'><a href="fixedBlades">Fixed Blades</a></li>
-//             <li className='help'><a href="slipJoints">Slip Joints</a></li>
-//             <li className='help'><a href="flippers">Flippers</a></li>
-//             <li className='help'><a href="automatics">Automatic</a></li>
-//             <li className='help'><a href="assisted">Assisted</a></li>
-//             <li className='help'><Link className='dashboardLinkStyling' to='yourKnives'>Add Knife</Link></li>
-//           </ul>
-//           </div>
-//         ) :
-//           (
-//             null
-//           )
-//         }
-//       </div>
-//     );
-//   }
-// }
+        {this.state.displayMenu ? (
+          <div className='help1'>
+          <ul>
+            <li className='help'><a href="dashboard">Dashboard</a></li>
+            <li className='help'><a href="fixedBlades">Fixed Blades</a></li>
+            <li className='help'><a href="slipJoints">Slip Joints</a></li>
+            <li className='help'><a href="flippers">Flippers</a></li>
+            <li className='help'><a href="automatics">Automatic</a></li>
+            <li className='help'><a href="assisted">Assisted</a></li>
+            <li className='help'><Link className='dashboardLinkStyling' to='yourKnives'>Add Knife</Link></li>
+          </ul>
+          </div>
+        ) :
+          (
+            null
+          )
+        }
+      </div>
+    );
+  }
+}
 
 
-// var displayDropdown = (
-//   <div>
-//     <Dropdown />
-//   </div>
-// );
+var displayDropdown = (
+  <div>
+    <Dropdown />
+  </div>
+);
 
-// ReactDOM.render(displayDropdown, document.getElementById('root'));
-// export default Dropdown;
+ReactDOM.render(displayDropdown, document.getElementById('root'));
+export default Dropdown;
--- src/components/fixedBladesList.js
+++ src/components/fixedBladesList.js
@@ -1,6 +1,6 @@
 import React, { } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import Dropdown from '../components/dropdown';
+// import Dropdown from '../components/dropdown';
 
 const CurrentDate = (props) => {
   var tempDate = new Date();
@@ -26,7 +26,8 @@ const Table = () => {
     <div>
       <table className='dashboardTableStyling'>
         <tbody>
-          <tr><td className='border'><Dropdown /></td>
+          <tr>
+            {/* <td className='border'><Dropdown /></td> */}
             <td className='border'><Link className='dashboardLinkStyling' to='forSale'>For Sale</Link></td>
             <td className='border'><Link className='dashboardLinkStyling' to='edcRotation'>EDC Rotation</Link></td>
             <td className='border'><Link className='dashboardLinkStyling' to='wishList'>Wish List</Link></td></tr>
